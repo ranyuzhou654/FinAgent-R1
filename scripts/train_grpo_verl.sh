7c3564039619befa7191b09090ee07b6e970f25d
ranyuzhou654 <927152344@qq.com>
2026-03-25 20:55:33 +0800

    Update train_grpo_verl.sh

--- scripts/train_grpo_verl.sh
+++ scripts/train_grpo_verl.sh
@@ -7,6 +7,7 @@ cd "$ROOT_DIR"
 export CUDA_VISIBLE_DEVICES="${CUDA_VISIBLE_DEVICES:-0}"
 export VLLM_ATTENTION_BACKEND="${VLLM_ATTENTION_BACKEND:-XFORMERS}"
 export WANDB_PROJECT="${WANDB_PROJECT:-FinAgent-R1}"
+export PYTHONPATH="${ROOT_DIR}:${ROOT_DIR}/vendor/Search-R1:${PYTHONPATH:-}"
 
 PYTHONUNBUFFERED=1 python training/finagent_verl_main.py "$@"
 
